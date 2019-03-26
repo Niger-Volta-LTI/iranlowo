94f2189886416b008bdfaa99d67ce9aa65041ce0
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-03-26 16:49:57 -0700

    [ADD] utf-8 chars to code to see how ish is handled

--- tests/test_adr_tools.py
+++ tests/test_adr_tools.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import filecmp
-import iranlowo
+import iranlowo as ránlọ
 import os
 
 def test_strip_accents():
@@ -9,9 +9,9 @@ def test_strip_accents():
     yo_0 = "ọjọ́ìbí 18 Oṣù Keje 1918 jẹ́ Ààrẹ Gúúsù Áfríkà"
     yo_1 = "Kí ó tó di ààrẹ"
 
-    assert iranlowo.adr_tools.strip_accents(ca_fr) == "Montreal, uber, 12.89, Mere, Francoise, noel, 889"
-    assert iranlowo.adr_tools.strip_accents(yo_0) == "ojoibi 18 Osu Keje 1918 je Aare Guusu Afrika"
-    assert iranlowo.adr_tools.strip_accents(yo_1) == "Ki o to di aare"
+    assert ránlọ.adr_tools.strip_accents(ca_fr) == "Montreal, uber, 12.89, Mere, Francoise, noel, 889"
+    assert ránlọ.adr_tools.strip_accents(yo_0) == "ojoibi 18 Osu Keje 1918 je Aare Guusu Afrika"
+    assert ránlọ.adr_tools.strip_accents(yo_1) == "Ki o to di aare"
 
 
 def test_strip_accents_from_file():
@@ -20,7 +20,7 @@ def test_strip_accents_from_file():
     target_file = cwd + "/tests/testdata/file2.txt"
     test_target_file = cwd + "/tests/testdata/file3.txt"
 
-    assert(iranlowo.adr_tools.strip_accents_from_file(src_file, test_target_file) is True)
+    assert(ránlọ.adr_tools.strip_accents_from_file(src_file, test_target_file) is True)
     assert(filecmp.cmp(src_file, test_target_file) is False)
     assert(filecmp.cmp(target_file, test_target_file))
 
