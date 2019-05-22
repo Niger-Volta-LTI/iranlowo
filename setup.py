eefad30c3a5041b3dc01ec75d21d79c40dac61da
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-05-22 01:11:03 -0700

    [ADD] bump w/ failing travis to 0.0.4, so we can upload sdist manually

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ url: https://github.com/Niger-Volta-LTI
 download_url: archive/v{version}.tar.gz
 
 package_data={'iranlowo': ['models/yo_adr_bahdanau_lstm_128_1_1_step_100000_release.pt']}
+versioning="distance",  # we start from 0.0.0 using v{major}.{minor}.0
+
 """
 
 
@@ -14,8 +16,8 @@ from setuptools import find_packages
 
 setup(
     name="iranlowo",
+    version='0.0.4',
     setup_requires="setupmeta",
-    versioning="distance",  # we start from 0.0.0 using v{major}.{minor}.0
     license="MIT",
     author="Ruoho Ruotsi ruoho.ruotsi@gmail.com",
     description="Utility package for analysis & (pre)processing of Yorùbá text",
