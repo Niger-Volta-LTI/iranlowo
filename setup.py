3a3926eb00489ce950df56a47cf196e5568ae946
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-05-26 20:21:55 -0700

    [FIX] broken Travis build, so all along the encrypted password was the github token, thinking that travis needed that to access github for building, but its in the pypi section and perhaps its what we need to login to pypi properly for the upload. So changed the encryption to be the pypi password. Take je-ne-sais-plus-moi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages
 
 setup(
     name="iranlowo",
-    version='0.0.5.1',
+    version='0.0.5.2',
     setup_requires="setupmeta",
     license="MIT",
     author="Ruoho Ruotsi ruoho.ruotsi@gmail.com",
