7db0ec49d6d7728ae1fad5ddc5102c934ed336b9
Olamyy <olamyy53@gmail.com>
2019-12-15 23:50:38 +0100

    Fixed failing tests

--- src/iranlowo/corpus/loaders.py
+++ src/iranlowo/corpus/loaders.py
@@ -2,10 +2,6 @@ import os
 
 from iranlowo.corpus import get_corpus
 
-os.environ[
-    "NIGER_VOLTA_CORPUS"
-] = "/Users/Olamilekan/Desktop/Machine Learning/OpenSource/yoruba-text"
-
 
 def niger_volta_corpus(corpus_code):
     return get_corpus(name=corpus_code, niger_volta=True)
